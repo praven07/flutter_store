e6476c6e7676f861f9db8c09b4c606233d6ca3eb
Praven <praven2010@live.com>
2024-01-11 15:36:59 -0600

    Updated the StoreBuilder
    Changes the api completely. Cleaned up the code and bugs. It is much more simpler to use.

--- lib/src/store_builder.dart
+++ lib/src/store_builder.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
-
 import 'package:flutter/widgets.dart';
 import 'package:store/store.dart';
 
-class StoreBuilder<@required T extends Store, @required S> extends StatefulWidget {
+class StoreBuilder<T extends Store, S> extends StatefulWidget {
 
+  /// The [Store] that the builder is connected to and listens
+  /// for state changes.
   final T store;
-  final Function(BuildContext, S) builder;
+
+  /// This builder is used to build [Widget] based on the state of the [Store]
+  final Widget Function(BuildContext, S) builder;
 
   const StoreBuilder({
     super.key,
@@ -15,35 +18,55 @@ class StoreBuilder<@required T extends Store, @required S> extends StatefulWidge
   });
 
   @override
-  State<StoreBuilder> createState() => _StoreBuilderState<T, S>();
+  State<StoreBuilder<T, S>> createState() => _StoreBuilderState<T, S>();
 }
 
-class _StoreBuilderState<@required T extends Store, @required S> extends State<StoreBuilder> {
-
-  late StreamSubscription _streamSubscription;
-  late S _state;
+class _StoreBuilderState<T extends Store, S>
+    extends State<StoreBuilder<T, S>> {
+  late S _stateSnapshot;
+  StreamSubscription<S>? _subscription;
 
   @override
   void initState() {
     super.initState();
-
-    _state = widget.store.state.value;
-
-    /// Subscribes to the store state stream and calls the builder methods when
-    /// state changes.
-    _streamSubscription = widget.store.state.listen((state) {
-      setState(() {
-        _state = state;
-      });
-    });
+    assert(
+    widget.store.initialState != null,
+    "The provided store does not have an initial state.",
+    );
+    _stateSnapshot = widget.store.initialState;
+    _subscribe();
   }
 
   @override
-  Widget build(BuildContext context) => widget.builder(context, _state);
+  void didUpdateWidget(covariant StoreBuilder<T, S> oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.store != widget.store) {
+      _unsubscribe();
+      _subscribe();
+    }
+  }
+
+  void _subscribe() {
+    _subscription = widget.store.state.listen((event) {
+      setState(() {
+        _stateSnapshot = event;
+      });
+    }) as StreamSubscription<S>?;
+  }
+
+  void _unsubscribe() {
+    if (_subscription != null) {
+      _subscription!.cancel();
+      _subscription = null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => widget.builder(context, _stateSnapshot);
 
   @override
   void dispose() {
-    _streamSubscription.cancel();
+    _unsubscribe();
     super.dispose();
   }
 }
